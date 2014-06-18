bc0f42a93df2f0d93a9976102defdb8e8c21a42c
Alan Jiang <ajiang@us.ibm.com>
2014-06-18 13:43:54 -0500

    missing change from Sync github pull request #114 , add list volumes changes
    Change-Id: Ie566c87d96bd89d799e9f39b5ec6785efc8b0086

--- jumpgate/volume/drivers/sl/__init__.py
+++ jumpgate/volume/drivers/sl/__init__.py
@@ -1,4 +1,4 @@
-from .volumes import VolumesV2, VolumesDetailV1
+from .volumes import VolumesV2, VolumesV1, VolumeV1
 from jumpgate.common.sl import add_hooks
 
 
@@ -9,7 +9,8 @@ def setup_routes(app, disp):
     disp.set_handler('v2_volumes_detail', VolumesV2())
 
     # V1 Routes
-    disp.set_handler('v1_volumes_detail', VolumesDetailV1())
-    disp.set_handler('v1_volume', VolumesDetailV1())
+    disp.set_handler('v1_volumes_detail', VolumesV1())
+    disp.set_handler('v1_volume', VolumeV1())
+    disp.set_handler('v1_volumes', VolumesV1())
 
     add_hooks(app)
